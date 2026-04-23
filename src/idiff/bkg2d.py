e8a8058c59dac6126ef1d9b870b20c26079e2041
Jakub David <jakubdavid64@gmail.com>
2026-04-23 14:26:31 +0200

    add neural network class

--- src/idiff/bkg2d.py
+++ src/idiff/bkg2d.py
@@ -7,6 +7,8 @@ Background subtraction for 2D-arrays/images.
 
 
 import skimage as sk
+import numpy as np
+import onnxruntime as ort
 
 
 def rolling_ball(arr, radius=20):
@@ -32,3 +34,21 @@ def rolling_ball(arr, radius=20):
     arr_bcorr = arr - background
     # Return array with subtracted background
     return(arr_bcorr)
+
+
+class NeuralNetwork:
+    def __init__(self, path):
+        self.model = ort.InferenceSession(path)
+
+    def predict(self, x):
+        if len(x.shape) == 2:
+            x = x[None, None]
+        elif len(x.shape) == 3:
+            x = x[:, None]
+        
+        if x.dtype != np.float32:
+            x = x.astype(np.float32)
+
+        clean, bkg = self.model.run(None, {"x": x})
+
+        return clean.squeeze(), bkg.squeeze()
